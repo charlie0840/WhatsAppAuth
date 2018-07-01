cdf12682c54f23fb1fbe59a272d9c0eed989eb58
charlie0840 <charlie0840@gmail.com>
2018-07-01 03:42:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/charlie0840/whatsappauth/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.charlie0840.whatsappauth
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
